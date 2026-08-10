69211c68b0d4023a1a444aece4e4881d39db8faa
Durgarao1208 <pinnintidurgarao123@gmail.com>
2026-08-10 15:37:58 +0530

    feat: initial commit

---
+++ src/main/java/io/github/pdr/ai/rag/RagPocApplication.java
@@ -0,0 +1,13 @@
+package io.github.pdr.ai.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pdr/ai/rag/RagPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdr.ai.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
